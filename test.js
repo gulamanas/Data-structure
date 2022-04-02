234b38d2b63cd7fdb6e0a662299dc3c4b44db130
Gulam Anas <g.anas786@gmail.com>
2022-04-02 23:04:31 +0530

    data structure

---
+++ test.js
@@ -0,0 +1,16 @@
+// let n = 10;
+// let x = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   x = i + x;
+// }
+// console.log(x);
+
+function sumOfNumbers(n) {
+  let x = 0;
+  for (let i = 0; i <= n; i++) {
+    x = i + x;
+  }
+  console.log(x);
+}
+sumOfNumbers(10);
