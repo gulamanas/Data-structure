a3ba2581b8b312b14b89e39f1e31636ca6c0fa6b
Gulam Anas <g.anas786@gmail.com>
2022-04-02 23:38:54 +0530

    testing performance

--- test.js
+++ test.js
@@ -13,11 +13,20 @@ function sumOfNumbers(n) {
   for (let i = 0; i <= n; i++) {
     x = i + x;
   }
-  console.log(x);
+  return x;
 }
 sumOfNumbers(10);
+
+var t1 = performance.now();
+sumOfNumbers(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
 
 function addUpTo(n) {
   return (n * (n + 1)) / 2;
 }
-console.log(addUpTo(10));
+// console.log(addUpTo(10));
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`);
