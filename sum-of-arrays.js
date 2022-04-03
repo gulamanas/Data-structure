dc49272b217285afdb9b1d7431a79438f9c06719
Gulam Anas <g.anas786@gmail.com>
2022-04-03 23:39:17 +0530

    sum of arrays

---
+++ sum-of-arrays.js
@@ -0,0 +1,11 @@
+function sumOfArrays() {
+  arr = [1, 3, 6, 2];
+  x = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    x = x + arr[i];
+  }
+  return x;
+}
+// sumOfArrays();
+console.log(sumOfArrays());
