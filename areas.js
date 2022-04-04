6626ee55aa797a10ac4abec29df59c6f1a9ad9a3
Gulam Anas <g.anas786@gmail.com>
2022-04-04 19:04:41 +0530

    area of rectagle

--- areas.js
+++ areas.js
@@ -2,6 +2,13 @@
 
 function triangleArea(b, h) {
   let area = 0.5 * b * h;
-  return `${area} sq m`;
+  return `${area} sq unit`;
 }
 console.log(triangleArea(4, 3));
+
+// area of rectagle
+function rectagleArea(l, h) {
+  let area = l * h;
+  return `${area} sq unit`;
+}
+console.log(rectagleArea(4, 5));
