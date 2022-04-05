d261207d20888dcc730dedda07ab527a4996fefe
Gulam Anas <g.anas786@gmail.com>
2022-04-05 23:54:44 +0530

    reverse an array

--- reverse-an-array.js
+++ reverse-an-array.js
@@ -1,5 +1,5 @@
 function reverseAnArray() {
-  let arr = [2, 4, 1, 5];
+  let arr = [2, 4, 1, 5, 10, 3, 6];
   let n = arr.length - 1;
 
   for (let i = 0; i <= n / 2; i++) {
@@ -11,13 +11,3 @@ function reverseAnArray() {
   console.log(arr);
 }
 reverseAnArray();
-
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-// let n = arr.length-1;
-
-// for(let i=0; i<=n/2; i++) {
-//   let temp = arr[i];
-//   arr[i] = arr[n-i];
-//   arr[n-i] = temp;
-// }
-// console.log(arr);
