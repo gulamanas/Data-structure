8022d56dfa60e286f27258603abed36d8a6f40a0
Gulam Anas <g.anas786@gmail.com>
2022-05-05 23:58:43 +0530

    count of digits

---
+++ countofDigits.js
@@ -0,0 +1,9 @@
+// Count of Digits in a number
+
+function countOfDigits(num) {
+  //   return num;
+  var r = num.toString();
+  return r.length;
+}
+result = countOfDigits(123456);
+console.log(result);
