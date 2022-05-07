51c04bf485aef986334bdcd0f847f594eb8cecbc
Gulam Anas <g.anas786@gmail.com>
2022-05-07 06:35:25 +0530

    largest digit in number

--- largestDigit.js
+++ largestDigit.js
@@ -11,10 +11,21 @@
 //     }
 // }
 
-function maxDigit(n) {
-  var a = n.toString();
-  var b = a.split("");
+// function maxDigit(n) {
+//   var a = n.toString();
+//   var b = a.split("");
 
-  return Math.max.apply(null, b);
+//   return Math.max.apply(null, b);
+// }
+// console.log(maxDigit(238445));
+
+// By using Recursion
+function maxDigit(n) {
+  if (n == 0) {
+    return 0;
+  } else {
+    var remainder = n % 10;
+    return Math.max(remainder, maxDigit((n - remainder) * 1e-1));
+  }
 }
-console.log(maxDigit(238445));
+console.log(maxDigit(16984));
