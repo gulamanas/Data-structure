910981f99aec56415dd6cdce374fb51b3c0653ec
Gulam Anas <g.anas786@gmail.com>
2022-05-08 22:44:56 +0530

    sqare root

---
+++ checkforsquare.js
@@ -0,0 +1,8 @@
+// Check if a number is a perfect square or not
+
+// using math inbuilt function of JavaScript
+
+function squareRoot(num) {
+  return Math.sqrt(num);
+}
+console.log(squareRoot(25));
